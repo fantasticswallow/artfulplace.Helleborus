dda9b35d163703f4661bd10f68debf9864121790
azusa minamori <f_swallow@live.jp>
2018-07-16 19:50:16 +0900

    import all

---
+++ artfulplace.Helleborus.Sample/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace artfulplace.Helleborus.Sample
+{
+    class Program
+    {
+        const string path = @"sample.txt";
+
+        static void Main(string[] args)
+        {
+            var tree = SyntaxHelper.ParseFileText(path);
+
+            foreach (var (element, type, nestedLevel) in SyntaxHelper.GetElements(tree))
+            {
+                ConsoleWrite(SyntaxHelper.GetSyntaxName(element, type), nestedLevel);
+            }
+
+            // var root = tree.GetRoot();
+            // ConsoleWrite(root, 1, true);
+            // SeekChildren(root.ChildNodesAndTokens(), 2);
+            // Console.ResetColor();
+        }
+
+        static void SeekChildren(IEnumerable<SyntaxNodeOrToken> children, int nestedLevel)
+        {
+            foreach (var child in children)
+            {
+                ConsoleWrite(child, nestedLevel, child.IsNode);
+                SeekChildren(child.ChildNodesAndTokens(), nestedLevel + 1);
+                SeekTrivia(child, nestedLevel + 1);
+            }
+        }
+
+        static void SeekTrivia(SyntaxNodeOrToken target, int nestedLevel)
+        {
+            if (target.HasLeadingTrivia)
+            {
+                foreach (var trivia in target.GetLeadingTrivia())
+                {
+                    ConsoleWrite(trivia, nestedLevel);
+                }
+            }
+
+            if (target.HasTrailingTrivia)
+            {
+                foreach (var trivia in target.GetTrailingTrivia())
+                {
+                    ConsoleWrite(trivia, nestedLevel);
+                }
+            }
+        }
+
+        static void ConsoleWrite(SyntaxNodeOrToken node, int nestedLevel, bool isNode = false)
+        {
+            var tabs = string.Concat(Enumerable.Range(0, nestedLevel).Skip(1).Select(x => "  ").ToArray());
+
+            if (isNode)
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+
+            Console.WriteLine($"{tabs}{SyntaxHelper.GetSyntaxName(node)}, {node.Span.ToString()}");
+        }
+
+        static void ConsoleWrite(SyntaxTrivia trivia, int nestedLevel)
+        {
+            var tabs = string.Concat(Enumerable.Range(0, nestedLevel).Skip(1).Select(x => "  ").ToArray());
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"{tabs}{trivia.Kind().ToString()} {trivia.Span.ToString()}");
+        }
+
+        static void ConsoleWrite(string name, int nestedLevel)
+        {
+            var tabs = string.Concat(Enumerable.Range(0, nestedLevel).Skip(1).Select(x => "  ").ToArray());
+            Console.WriteLine($"{tabs}{name} ");
+        }
+    }
+}
---
+++ artfulplace.Helleborus/SyntaxHelper.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace artfulplace.Helleborus
+{
+    public class SyntaxHelper
+    {
+        public static CSharpSyntaxTree ParseFileText(string filePath)
+        {
+            using (var sr = new StreamReader(filePath))
+            {
+                return (CSharpSyntaxTree)CSharpSyntaxTree.ParseText(sr.ReadToEnd(), CSharpParseOptions.Default);
+            }
+        }
+
+        public static CSharpSyntaxTree ParseText(string document)
+        {
+            return (CSharpSyntaxTree)CSharpSyntaxTree.ParseText(document, CSharpParseOptions.Default);
+        }
+
+        public static string GetSyntaxName(SyntaxNodeOrToken target)
+        {
+            if (target.IsNode)
+            {
+                var node = (SyntaxNode)target;
+                return node.GetType().Name;                
+            }
+            else if (target.IsToken)
+            {
+                var token = (SyntaxToken)target;
+                return token.Kind().ToString();
+            }
+
+            return "Detection Error!";
+        }
+
+        public static string GetSyntaxName(object target, ElementType type)
+        {
+            switch (type)
+            {
+                case ElementType.SyntaxNode:
+                    return ((SyntaxNode)target).GetType().Name;
+                case ElementType.SytaxToken:
+                    return ((SyntaxToken)target).Kind().ToString();
+                case ElementType.SyntaxTrivia:
+                    return ((SyntaxTrivia)target).Kind().ToString();
+                default:
+                    return "Detection Error!";
+            }
+        }
+
+        public static IEnumerable<(object element, ElementType type, int nestedLevel)> GetElements(CSharpSyntaxTree target)
+        {
+            var root = target.GetRoot();
+            yield return (root, ElementType.SyntaxNode, 1);
+            foreach (var element in GetChildElements(root.ChildNodesAndTokens(), 2))
+            {
+                yield return element;
+            }
+        }
+
+        static IEnumerable<(object element, ElementType type, int nestedLevel)> GetChildElements(IEnumerable<SyntaxNodeOrToken> targets, int nestedLevel)
+        {
+            foreach (var child in targets)
+            {
+                if (child.IsNode)
+                {
+                    yield return ((SyntaxNode)child, GetElementType(child.IsNode), nestedLevel);
+                }
+                else
+                {
+                    yield return ((SyntaxToken)child, GetElementType(child.IsNode), nestedLevel);
+                }
+                
+                foreach (var obj in GetChildElements(child.ChildNodesAndTokens(), nestedLevel + 1))
+                {
+                    yield return obj;
+                }
+
+                foreach (var obj in GetTrivia(child, nestedLevel + 1))
+                {
+                    yield return obj;
+                }
+            }
+        }
+
+        static IEnumerable<(object element, ElementType type, int nestedLevel)> GetTrivia(SyntaxNodeOrToken target, int nestedLevel)
+        {
+            if (target.HasLeadingTrivia)
+            {
+                foreach (var trivia in target.GetLeadingTrivia())
+                {
+                    yield return (trivia, ElementType.SyntaxTrivia, nestedLevel);
+                }
+            }
+
+            if (target.HasTrailingTrivia)
+            {
+                foreach (var trivia in target.GetTrailingTrivia())
+                {
+                    yield return (trivia, ElementType.SyntaxTrivia, nestedLevel);
+                }
+            }
+        }
+
+        static ElementType GetElementType(bool isNode) => isNode ? ElementType.SyntaxNode : ElementType.SytaxToken;
+        
+        public enum ElementType
+        {
+            SyntaxNode,
+            SytaxToken,
+            SyntaxTrivia,
+        }
+
+        public static IEnumerable<(string key, string value)> GetElementProperties(object element)
+        {
+            var type = element.GetType();
+            foreach (var prop in type.GetProperties())
+            {
+                yield return (prop.Name, prop.GetValue(element).ToString());
+            }
+        }
+    }
+}
